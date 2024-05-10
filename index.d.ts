b81f862efe78bc8ebcc22983d7e3263a586ecf7b
feng <1984779164@qq.com>
2024-05-11 00:48:10 +0800

    fix: minIO 配置类型修复

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,15 @@
+/*
+ * @Date: 2024-05-10 12:13:58
+ * @Author: 枫
+ * @LastEditors: 枫
+ * @description: description
+ * @LastEditTime: 2024-05-11 00:40:59
+ */
 import { ClientOptions } from 'minio';
 export * from './dist/index';
 
 declare module '@midwayjs/core/dist/interface' {
   interface MidwayConfig {
-    minIO?: ClientOptions;
+    minIO?: ServiceFactoryConfigOption<ClientOptions>;
   }
 }
